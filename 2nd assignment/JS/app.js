eaf34a0c282e2e015c3c4cbc5d77c5167dcab57b
Taheera Raza Turk <taheeraraza0@gmail.com>
2024-10-05 08:12:54 -0700

    Add files via upload

---
+++ 2nd assignment/JS/app.js
@@ -0,0 +1,26 @@
+// 1. Write a script to greet your website visitor using JS alert box.
+  
+     alert("Welcome to 'MY WEBSITE'..!");
+
+// 2. Write a script to display following message on your web page:]
+
+    alert("Error! Please enter a vaild password.");
+
+ // 3. Write a script to display following message on your web page:
+
+     alert("Welcome to JS Land... \n Happy Coding!");
+
+// 4. Write a script to display following messages in sequence:
+
+     alert("Welcome to JS Land...");
+     alert("Happy Coding! \n  ◻Prevent this page from creating additional dialogs.");
+
+ // 5. Generate the following message through browser’s developer console:
+
+     alert("Hello! I can run JS through my web browser's console");
+
+
+  
+
+ 
+
